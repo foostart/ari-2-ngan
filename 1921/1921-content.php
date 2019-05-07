b48622526b88bf5a30e753c5f6f29f6d0fb8da65
Trần Thị Kim Ngân <ttkimngan041098@gmail.com>
2019-05-08 01:38:57 +0700

    Update

--- 1921/1921-content.php
+++ 1921/1921-content.php
@@ -8,43 +8,46 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 
-<div class="type-1921">
-    <div class="container">
-        <div class="row img">
-            <div class="col-md-12 slide-band6">
-                <div class="col-md-8 img1">
-                </div>
-                <div class="col-md-4 img2">
-                    <div class="wrapper">
-                        <div class="single-img item-adv-simple item1">
-                            <div class="wrapper item-adv-simple">
-                                <a href="#">
-                                    <img src="images/ad1-2.jpg" class="img-responsive">
-                                </a>
-                            </div>
 
-                            <div class="promotion info-2">
-                                <h3>LG 8803</h3>
-                                <p>Aliquam tempus rutum</p>
-                                <span class="money">
-                                    <span class="icon">$</span>
-                                    350.00
-                                </span>
-                            </div>
-                        </div>
+<div class="container">
+    <div class="row">
+        <div class="type-1921">
+            <div class="col-md-4">
+                <div class="img">
+                    <div class="slide-band6">
+                        <div class="img2">
+                            <div class="wrapper">
+                                <div class="single-img item-adv-simple item1">
+                                    <div class="wrapper item-adv-simple">
+                                        <a href="#">
+                                            <img src="<?php echo $url_path ?>/images/ad1-2.jpg" class="img-responsive">
+                                        </a>
+                                    </div>
 
-                        <div class="single-img item-adv-simple item2">
-                            <div class="wrapper item-adv-simple ">
-                                <a href="#">
-                                    <img src="images/ad2-2.jpg" class="img-responsive">
-                                </a>
-                                <div class="promotion info-3">
-                                    <h3>iPOD APPLE</h3>
-                                    <p>Aliquam tempus rutum</p>
-                                    <span class="money">
-                                        <span class="icon">$</span>
-                                        170.00
-                                    </span>
+                                    <div class="promotion info-2">
+                                        <h3>LG 8803</h3>
+                                        <p>Aliquam tempus rutum</p>
+                                        <span class="money">
+                                            <span class="icon">$</span>
+                                            350.00
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="single-img item-adv-simple item2">
+                                    <div class="wrapper item-adv-simple ">
+                                        <a href="#">
+                                            <img src="<?php echo $url_path ?>/images/ad2-2.jpg" class="img-responsive">
+                                        </a>
+                                    </div>
+                                    <div class="promotion info-3">
+                                        <h3>iPOD APPLE</h3>
+                                        <p>Aliquam tempus rutum</p>
+                                        <span class="money">
+                                            <span class="icon">$</span>
+                                            170.00
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
